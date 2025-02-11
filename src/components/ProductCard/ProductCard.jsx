cbf46710634a7915e17186d1916a44d298fcea13
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2025-02-11 17:42:46 +0600

    feat: add handle cart

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProductCard.module.css";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import AddIcon from '@mui/icons-material/Add';
-import { useDispatch } from "react-redux";
-import { addToCart } from "../../store/slice/CartSlice";
+import DoneIcon from '@mui/icons-material/Done';
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../../store/slice/CartSlice";
 
 const ProductCard = ({ product }) => {
-    const dispatch = useDispatch()
+    const [isAdded, setIsAdded] = useState(false);
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems);
+
+    useEffect(() => {
+        const productInCart = cartItems.find(item => item.id === product.id);
+        setIsAdded(!!productInCart);
+    }, [cartItems, product.id]);
+
+    const handleAddToCart = () => {
+        dispatch(addToCart(product));
+        setIsAdded(true);
+    };
+
+    const handleRemoveFromCart = () => {
+        dispatch(removeFromCart(product.id));
+        setIsAdded(false);
+    };
+
     return (
         <div className={styles.card}>
             <FavoriteBorderOutlinedIcon className={styles.favorite} />
-            <img src={product.images[0]} alt={product.title} className={styles.image} />
+            <img
+                src={product.images[0]}
+                alt={product.title}
+                className={styles.image}
+                style={{ width: "113px", height: "112px" }}
+            />
             <h3 className={styles.name}>{product.title}</h3>
             <div className={styles.card_bottom}>
                 <div className={styles.card_price}>
                     <p className={styles.price}>Цена:</p>
                     <p className={styles.price_name}>{product.price}</p>
                 </div>
-                <button className={styles.addButton} onClick={() => dispatch(addToCart(product))}>  <AddIcon /></button>
+                <button
+                    className={isAdded ? styles.addedButton : styles.addButton}
+                    onClick={isAdded ? handleRemoveFromCart : handleAddToCart}
+                    disabled={isAdded}
+                >
+                    {isAdded ? <DoneIcon /> : <AddIcon />}
+                </button>
             </div>
         </div>
     );
